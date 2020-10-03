7653673063a5dc14ebbd93b32e315ca546bad3ab
Christoph Vögele <christoph228@gmail.com>
2020-10-04 01:57:03 +0200

    sample Mutlithread

--- src/main/java/ch/voegele/CornellBox.java
+++ src/main/java/ch/voegele/CornellBox.java
@@ -3,12 +3,19 @@ package ch.voegele;
 import ch.voegele.util.Vec2;
 import ch.voegele.util.Vec3;
 import javafx.application.Application;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableArrayBase;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.awt.image.BufferedImage;
+import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class CornellBox extends Application {
 
@@ -18,20 +25,37 @@ public class CornellBox extends Application {
     Vec3 eye = new Vec3(0, 0, -4);
     Vec3 lookAt = new Vec3(0, 0, 6);
     double FOV = 36;
+    static int numberOfThreads = 1;
+    static int sampleRate = 1;
+
+    private PixelWriter writer;
+    private Vec3[][] imageArray;
 
     Scene scene;
 
     public static void main(String[] args) {
+        if (args.length > 0) {
+            var input = args[0];
+            var splits = input.split("-");
+            numberOfThreads = Integer.parseInt(splits[0]);
+            sampleRate = Integer.parseInt(splits[1]);
+        } else {
+            numberOfThreads = 4;
+            sampleRate = 10;
+        }
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws InterruptedException {
         initScene();
-        ImageView view = RenderCornellBox(4096, 24);
+        ImageView view = RenderCornellBox(sampleRate, numberOfThreads);
         javafx.scene.Scene scene = new javafx.scene.Scene(new VBox(view), width, height);
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        Timer timer = new Timer();
+        timer.schedule(new UpdateWindow(), 1000, 2000);
     }
 
     private void initScene() {
@@ -49,19 +73,21 @@ public class CornellBox extends Application {
 //        scene = new CornellBox.Scene(new CornellBox.SceneElement[]{ lightBlueBall});
     }
 
-    private ImageView RenderCornellBox(int sampleRate, int numberOfThreads) {
+    private ImageView RenderCornellBox(int sampleRate, int numberOfThreads) throws InterruptedException {
         WritableImage image = new WritableImage(width, height);
-        PixelWriter writer = image.getPixelWriter();
+        writer = image.getPixelWriter();
 
         MyRenderer renderer = new MyRenderer(eye, lookAt, FOV);
+        imageArray = new Vec3[height][width];
 
         //go through all pixels
         var parts = height/numberOfThreads;
+        var threads = new Thread[numberOfThreads];
         for (int partIndex = 0; partIndex < numberOfThreads; partIndex++) {
             var start = parts * partIndex;
             var end = parts * partIndex + parts;
 
-            Thread t = new Thread(()->{
+            threads[partIndex] = new Thread(()->{
                 for (int v = start; v < end; v++) {
                     for (int u = 0; u < width; u++) {
                         double y = (((double) v / height) * 2) - 1;
@@ -86,20 +112,34 @@ public class CornellBox extends Application {
 //                blue = Math.pow(blue, 1 / 22d);
 //                green = Math.pow(green, 1 / 22d);
                         Vec3 finalColor = new Vec3(red * 255, green * 255, blue * 255);
+                        imageArray[u][v] = finalColor;
+                        //writer.setColor(u, v, Color.rgb(clamp(finalColor.x), clamp(finalColor.y), clamp(finalColor.z)));
 
-                        writer.setColor(u, v, Color.rgb(clamp(finalColor.x), clamp(finalColor.y), clamp(finalColor.z)));
                     }
                 }
             });
-            t.start();
+            threads[partIndex].start();
         }
-
-
 
         ImageView view = new ImageView();
         view.setImage(image);
         return view;
     }
+
+    private class UpdateWindow extends TimerTask {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    var finalColor = imageArray[j][i];
+                    if (finalColor != null)
+                        writer.setColor(j, i, Color.rgb(clamp(finalColor.x), clamp(finalColor.y), clamp(finalColor.z)));
+                }
+            }
+        }
+    }
+
 
     private int clamp(float a) {
         if (a > 255) return 255;
--- src/main/java/ch/voegele/Main.java
+++ src/main/java/ch/voegele/Main.java
@@ -2,6 +2,7 @@ package ch.voegele;
 
 public class Main {
     public static void main(String[] args) {
+
         CornellBox.main(args);
     }
 }
