bd7da7a01e32375b9ee388a7ebac34343883c345
Christoph Voegele <christoph228@gmail.com>
2020-10-04 13:46:35 +0200

    add simple BDRF

--- src/main/java/ch/voegele/Sphere.java
+++ src/main/java/ch/voegele/Sphere.java
@@ -8,12 +8,14 @@ public class Sphere implements SceneElement {
     private final float radius;
     private final Vec3 color;
     private final Vec3 Emission;
+    private final Vec3 specularColor;
 
-    public Sphere(Vec3 position, float radius, Vec3 color, Vec3 Emission) {
+    public Sphere(Vec3 position, float radius, Vec3 color, Vec3 Emission, Vec3 specularColor) {
         this.position = position;
         this.radius = radius;
         this.color = color;
         this.Emission = Emission;
+        this.specularColor = specularColor;
     }
 
     public Vec3 getPosition() {
@@ -30,5 +32,9 @@ public class Sphere implements SceneElement {
 
     public Vec3 getEmission() {
         return Emission;
+    }
+
+    public Vec3 getSpecularColor() {
+        return specularColor;
     }
 }
