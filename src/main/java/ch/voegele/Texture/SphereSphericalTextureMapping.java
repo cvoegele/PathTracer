da5262c20a62ef766918c1b5bc93e7a9a2cdaad6
Christoph Vögele <christoph228@gmail.com>
2020-10-09 15:00:02 +0200

    add spherical TextureMapping

---
+++ src/main/java/ch/voegele/Texture/SphereSphericalTextureMapping.java
@@ -0,0 +1,37 @@
+package ch.voegele.Texture;
+
+import ch.voegele.Point;
+import ch.voegele.util.MathUtilities;
+import ch.voegele.util.Vec3;
+
+import java.io.IOException;
+
+public class SphereSphericalTextureMapping extends AbstractBitmapTexture implements ITextureMapper {
+
+    public SphereSphericalTextureMapping(String path) throws IOException {
+        setTexture(path);
+    }
+
+    @Override
+    public int getColorByNormal(Vec3 normal) {
+        normal = normal.normalize();
+
+        double s = Math.atan2(normal.x, normal.z) / Math.PI + 1; //-PI up to PI
+        double t = Math.acos(normal.y) / Math.PI; //0 up to PI
+
+        s = MathUtilities.clamp(s, 0, 1);
+        t = MathUtilities.clamp(t, 0, 1);
+
+        int w = texture.getWidth();
+        int h = texture.getHeight();
+
+        int v = (int) (h * t);
+        int u = (int) (w * s);
+        v = MathUtilities.clamp(v, 0, h - 1);
+        u = MathUtilities.clamp(u, 0, w - 1);
+
+        var point = new Point(u, v);
+
+        return texture.getRGB(point.i1, point.i2);
+    }
+}
